a9bafad03114713bc19fb69c0b4b37132e19d852
adityadas20 <aditya.das2002.20.09@gmail.com>
2023-02-07 01:17:37 +0530

    fixing cors policy in app.js

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const cookieParser = require('cookie-parser');
 
 app.use(cookieParser());
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 app.use(cors({ origin: '*', credentials: true }));
 

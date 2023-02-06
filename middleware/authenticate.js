dac4e8b88ed64a0ae697889a4122479ec6b2470c
adityadas20 <aditya.das2002.20.09@gmail.com>
2023-02-07 02:31:00 +0530

    fixing cors policy in app.js

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -4,7 +4,13 @@ const User = require('../model/userSchema')
 const Authenticate = async (req, res, next) => {
     try {
         const token = req.cookies.jwtoken;
+        if (!token)
+            throw new Error('couldnt find token')
+
         const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
+        if (!verifyToken)
+            throw new Error('couldnt verify token')
+
         const rootUser = await User.findOne({ _id: verifyToken._id, "tokens.token": token });
         if (!rootUser)
             throw new Error('User not found')

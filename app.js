5785d4a71be49ed887f63036ebd4f59f044f9279
adityadas20 <aditya.das2002.20.09@gmail.com>
2023-02-07 01:24:49 +0530

    fixing cors policy in app.js

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const cookieParser = require('cookie-parser');
 
 app.use(cookieParser());
 
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "https://imaginative-mousse-3be478.netlify.app");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(cors({ origin: 'https://imaginative-mousse-3be478.netlify.app', credentials: true }));
 
